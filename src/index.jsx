0f6ae29923813c661893ef631cecdef79964d615
Maxon Corvil <corvil.mc@gmail.com>
2023-08-16 22:32:27 -0400

    updating homepage for gitpages

--- src/index.jsx
+++ src/index.jsx
@@ -2,15 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
     <React.StrictMode>
-      <Routes>
-        <Route path="/" element={<App />}></Route>
-      </Routes>
+        <App />
     </React.StrictMode>
-  </BrowserRouter>
 );
